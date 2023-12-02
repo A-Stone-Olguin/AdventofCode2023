13e4cf905b9e8bcfae99009ecda2ab02c95f2004
Stone Olguin <aolguin1@uwyo.edu>
2023-12-01 22:48:48 -0700

    Finished Day_02

--- src/day_02/day_02_part_1.rs
+++ src/day_02/day_02_part_1.rs
@@ -1,4 +1,58 @@
 use crate::helper_functions::io::*;
+use regex::Regex;
+
+// Compare the colors to the potential comparison values
+fn compare_colors(value : &str) -> bool {
+    // Comparison values for each color
+    let comp_red = 12;
+    let comp_green = 13;
+    let comp_blue = 14;
+
+    // Split out the matched color and value pair
+    let (num, color) = match value.split_once(' ') {
+        Some((v, c)) => (v.parse::<i32>().unwrap(),c),
+        None => (0, ""),
+    };
+
+    // Returns if the color fits the comparison value
+    match color {
+        "green" => num <= comp_green,
+        "red" => num <= comp_red,
+        "blue" => num <= comp_blue,
+        _ => false
+    }
+
+}
+
+// Returns the game number if it is valid
+fn get_valid_game_number(line_string: String) -> i32 {
+    // Get the Game number
+    let re = match Regex::new(r"Game (\d+):") {
+        Ok(r) => r,
+        Err(msg) => panic!("Error: {}", msg),
+    };
+    let mut game_num : i32 = 0; 
+    for (_, [digit]) in re.captures_iter(&line_string).map(|c| c.extract()) {
+        game_num = digit.parse::<i32>().unwrap();
+    }
+
+    // Get each color and their value
+    let re_color = match Regex::new(r"(\d+ blue)|(\d+ green)|(\d+ red)") {
+        Ok(r) => r,
+        Err(msg) => panic!("Error: {}", msg),
+    };
+
+    for (_, [game]) in re_color.captures_iter(&line_string).map(|c| c.extract()) {
+        // If the colors fail a comparison, return the additive identity
+        if !compare_colors(game) {
+            return 0;
+        }
+    }
+    game_num
+
+
+}
+
 
 // Main function for day 2
 pub fn main() {
@@ -6,12 +60,15 @@ pub fn main() {
     let filename = "src/day_02/day_02_input.txt";
     // let filename = "src/day_02/test_01.txt";
 
+    let mut sum = 0;
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                println!("{}", ip);
+                // Add the game id for each valid game
+                sum += get_valid_game_number(ip);
             }
         }
     }
+    println!("The final value of possible games is: {}", sum)
 }
--- src/day_02/day_02_part_2.rs
+++ src/day_02/day_02_part_2.rs
@@ -1,4 +1,50 @@
 use crate::helper_functions::io::*;
+use regex::Regex;
+
+// Calculates the set's power
+fn calculate_set_power(line_string: String) -> i32 {
+    // Grab each color and their values
+    let re_color = match Regex::new(r"(\d+ blue)|(\d+ green)|(\d+ red)") {
+        Ok(r) => r,
+        Err(msg) => panic!("Error: {}", msg),
+    };
+
+    // Grab each set of values, will eventually find the maximum
+    let mut green_vals = vec![];
+    let mut red_vals = vec![];
+    let mut blue_vals = vec![];
+    for (_, [game]) in re_color.captures_iter(&line_string).map(|c| c.extract()) {
+        // Split out each color and its associated value
+        let (num, color) = match game.split_once(' ') {
+            Some((v, c)) => (v.parse::<i32>().unwrap(),c),
+            None => (0, ""),
+        };
+        // Push the corresponding value to its vector
+        match color {
+            "green" => green_vals.push(num),
+            "red" => red_vals.push(num),
+            "blue" => blue_vals.push(num),
+            _ => ()
+        }
+    }
+    // Calculate each color's maximum to determine the power
+    let max_red = match red_vals.iter().max() {
+        Some(max) => max,
+        None => &1, // An error, put to multiplicative identity to not affect power calculation
+    };
+    let max_blue = match blue_vals.iter().max() {
+        Some(max) => max,
+        None => &1, // An error, put to multiplicative identity to not affect power calculation
+    };
+    let max_green = match green_vals.iter().max() {
+        Some(max) => max,
+        None => &1, // An error, put to multiplicative identity to not affect power calculation
+    };
+    max_red*max_blue*max_green
+
+
+
+}
 
 // Main function for day 2
 pub fn main() {
@@ -6,12 +52,15 @@ pub fn main() {
     let filename = "src/day_02/day_02_input.txt";
     // let filename = "src/day_02/test_02.txt";
 
+    let mut sum = 0;
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
-                println!("{}", ip);
+                // Sum up each game's power
+                sum += calculate_set_power(ip);
             }
         }
     }
+    println!("The final value of powers' sum is: {}", sum)
 }
